8a3a700d4e6aca5b342d7c0542835b378b589919
elicul <enzo.licul@yahoo.com>
2018-08-11 15:27:22 +0200

    sql and auth added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PhotoModule } from './photo/photo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot(),
+    PhotoModule,
+  ],
 })
 export class AppModule {}
